432efe685bb6fb6d64c3ee5ca6b0e925ba82242a
molinaadv <alexandre.brito@molinaadvogado.adv.br>
2026-06-18 13:53:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -316,6 +316,7 @@ def aplicar_css_sidebar_desktop():
     line-height: 1;
     font-weight: 950;
     letter-spacing: -2px;
+    color: #FFFFFF !important;
 }
 
 .sidebar-cap {
@@ -802,12 +803,11 @@ st.sidebar.markdown(
     """
     <div class="sidebar-brand">
         <div class="sidebar-v360">
-            <span style="color:#18BDF2 !important;">V</span><span style="color:#FFFFFF !important;">360</span>
-        </div>
-
-        <div class="sidebar-cap">
-            CAPTAÇÃO
-        </div>
+    <span style="color:#18BDF2">V</span>360
+</div>
+<div class="sidebar-cap">
+    CAPTAÇÃO
+</div>
     </div>
     """,
     unsafe_allow_html=True,
